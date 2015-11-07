4e6fd1daa327be7a15545eadcb135b995076890a
unknown <hasancelik@NBS213960832.sehir.edu.local>
2015-11-07 15:36:19 +0300

    init

---
+++ AntolojiContext.js
@@ -0,0 +1,55 @@
+﻿var http = require("http");
+var cheerio = require('cheerio');
+
+function getPoem(url, callback) {
+    http.get(url, function (res) {
+        var poem = {};
+        var data = '';
+        res.on("data", function (chunk) {
+            data += chunk;
+        });
+        res.on('end', function () {
+            var $ = cheerio.load(data);
+            poem.title = $('.Siir_baslik').text();
+            poem.text = $('.Siir_metin').text();
+            callback(poem);
+        });
+    }).on("error", function (e) {
+        console.log("Got error: " + e.message);
+    });
+}
+
+function getAllPoemLinkOfPoet(poetLink, callback, pagenumber) {
+    pagenumber = pagenumber || 1;
+    http.get(poetLink + 'siirleri/ara-/sirala-/sayfa-' + pagenumber + '/', function (res) {
+        var data = '';
+        res.on("data", function (chunk) {
+            data += chunk;
+        });
+        res.on('end', function () {
+            var $ = cheerio.load(data);
+            var poemLinks = $('.liste_border a').not('.liste_baslik .liste_border');
+            if (poemLinks.length > 0) {
+                getAllPoemLinkOfPoet(poetLink, callback, ++pagenumber);
+                poemLinks.each(
+                    function () {
+                        callback('http://www.antoloji.com' + $(this).attr('href'));
+                    });
+            }
+        });
+    }).on("error", function (e) {
+        console.log("Got error: " + e.message);
+    });
+}
+
+function getAllPoemOfPoet(poetLink, callback) {
+    getAllPoemLinkOfPoet(poetLink, function (link) {
+        getPoem(link, function (poem) {
+            callback(poem);
+        });
+    });
+}
+
+exports.getAllPoemLinkOfPoet = getAllPoemLinkOfPoet;
+exports.getPoem = getPoem;
+exports.getAllPoemOfPoet = getAllPoemOfPoet; 
---
+++ app.js
@@ -0,0 +1,19 @@
+﻿var ant = require('./AntolojiContext');
+
+//ant.getPoem('http://www.antoloji.com/balkon-siiri/', function (poem) {
+//    console.log('');
+//    console.log(poem.title);
+//    console.log('------------------------------------------');
+//    console.log(poem.text);
+//});
+
+//ant.getAllPoemLinkOfPoet('http://www.antoloji.com/necip-fazil-kisakurek/', function(link) {
+//    console.log(link);
+//});
+
+ant.getAllPoemOfPoet('http://www.antoloji.com/necip-fazil-kisakurek/', function (poem) {
+    console.log();
+    console.log(poem.title);
+    console.log('========================================');
+    console.log(poem.text);
+});
